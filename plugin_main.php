55a40430080121a06671b4e10185ceed8c93fc0d
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-05-12 11:11:51 +0100

    show resolved URI

--- plugin_main.php
+++ plugin_main.php
@@ -11,24 +11,33 @@
 	
 ?>
 <h2>Network Services  &rsaquo; Overview</h2>
+<div style="margin-bottom: 15px;">
+		<strong><?php echo $nodeUri; ?></strong>
+</div>
+
 <?php if($commsError):?>
 	<div class="notice notice-error"><p>There is an authentication error. Please check that your remote API token is correct</p></div>
 <?php endif; ?>
-<table>
-<?php
-	$cpage = filter_input(INPUT_GET, 'page');
-	foreach($services as $service):
-?>
-	<tr>
-		
-		<td>
-			<a href="?<?php echo "page=$cpage&service={$service['module']}"; ?>">
-			<?php echo $service['title']; ?>
-			</a>
-		</td>
-		<td><?php echo $service['description']; ?></td>
-	</tr>
-<?php
-	endforeach;
-?>
-</table>
+<div style="margin-right: 20px;">
+
+	<table class="wp-list-table widefat  striped">
+		<tbody class="the-list">
+	<?php
+		$cpage = filter_input(INPUT_GET, 'page');
+		foreach($services as $service):
+	?>
+		<tr>
+
+			<td>
+				<a href="?<?php echo "page=$cpage&service={$service['module']}"; ?>">
+					<strong><?php echo $service['title']; ?></strong>
+				</a>
+			</td>
+			<td><?php echo $service['description']; ?></td>
+		</tr>
+	<?php
+		endforeach;
+	?>
+		</tbody>
+	</table>
+</div>
