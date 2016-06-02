67a3331b794efa65f032508e3f79c72bc65c8392
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-06-02 15:23:34 +0100

    update: Update tested

--- plugin/update.php
+++ plugin/update.php
@@ -1,5 +1,6 @@
 <?php
 add_filter ('pre_set_site_transient_update_plugins', 'springdvs_update_plugin_check');
+add_filter('plugins_api', 'springdvs_plugin_update_info', 10, 3);
 
 function springdvs_update_plugin_check ($aggregate)
 {
@@ -17,10 +18,27 @@ function springdvs_update_plugin_check ($aggregate)
 	$obj->plugin = 'springdvs';
 	$obj->new_version = $info['version'];
 	$obj->url = 'http://www.spring-dvs.org';
-	$obj->tested = '4.5.2';
+	$obj->tested = $info['tested'];
 	$obj->package = 'http://packages.spring-dvs.org/wp.springdvs_'.$info['version'].".zip";
+	$obj->sections = $info['sections'];
 	$aggregate->response['springdvs/springdvs.php'] = $obj;
 	
 	return $aggregate;
 }
 
+function springdvs_plugin_update_info() {
+	$response = wp_remote_get("http://spring-dvs.org/versions/wp.springdvs.json");
+	$json = $response['body'];
+	$info = json_decode($json, true);
+
+	$obj = new stdClass();
+	$obj->slug = 'springdvs';
+	$obj->plugin = 'springdvs';
+	$obj->new_version = $info['version'];
+	$obj->url = 'http://www.spring-dvs.org';
+	$obj->tested = $info['tested'];
+	$obj->package = 'http://packages.spring-dvs.org/wp.springdvs_'.$info['version'].".zip";
+	$obj->sections = $info['sections'];
+	
+	return $obj;
+}
